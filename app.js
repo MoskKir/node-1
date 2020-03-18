01a8a0893439c4bc6ba37aabaf85a288ac417bda
Kirill Moskalevich <mosk9364@gmail.com>
2020-03-18 10:15:14 +0300

    feat: add base server

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function (request, response) {
+    console.log(`Запрошенный адрес: ${request.url}`);
+    if (request.method !== 'GET') {
+        response.statusCode = 400;
+        return response.end("Bad request!");
+    }
+    // получаем путь после слеша
+    const filePath = request.url.substr(1);
+    // смотрим, есть ли такой файл
+    fs.access(filePath, fs.constants.R_OK, err => {
+        // если произошла ошибка - отправляем статусный код 404
+        if (err) {
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }
+        else {
+            fs.createReadStream(filePath).pipe(response);
+        }
+    });
+}).listen(3000, function () {
+    console.log("Server started at http://localhost:3000");
+});
