ae181a975158d89d3760a7924d8a0a22e396859e
Kirill Moskalevich <mosk9364@gmail.com>
2020-03-19 10:32:56 +0300

    feat: add put request for users

--- node2/index.js
+++ node2/index.js
@@ -8,22 +8,24 @@ const jsonParser = bodyParser.json()
 let fileContent = fs.readFileSync('data.json', 'utf-8');
 // console.log(fileContent);
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+
 // получение списка данных
 app.get("/api/users", function(req, res){      
-    const content = fs.readFileSync("data.json", "utf8");
+    const content = fs.readFileSync('data.json', 'utf8');
     const users = JSON.parse(content);
     res.send(users);
 });
+
 // получение отправленных данных
-app.post("/api/users", jsonParser, (req, res) => {     
+app.post('/api/users', jsonParser, (req, res) => {     
     if(!req.body) return res.sendStatus(400);
 
     const userName = req.body.name;
     const userAge = req.body.age;
     const user = {name: userName, age: userAge};
      
-    let data = fs.readFileSync("data.json", "utf8");
+    let data = fs.readFileSync('data.json', 'utf8');
     let users = JSON.parse(data);
      
     // находим максимальный id
@@ -34,8 +36,36 @@ app.post("/api/users", jsonParser, (req, res) => {
     users.push(user);
     data = JSON.stringify(users);
     // перезаписываем файл с новыми данными
-    fs.writeFileSync("data.json", data);
+    fs.writeFileSync('data.json', data);
     res.send(user);
+});
+
+app.put('/api/users', jsonParser, function(req, res){
+      
+    if(!req.body) return res.sendStatus(400);
+     
+    const userId = req.body.id;
+    const userName = req.body.name;
+    const userAge = req.body.age;
+     
+    let data = fs.readFileSync('data.json', 'utf8');
+    const users = JSON.parse(data);
+    let user;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === +userId) {
+            user = users[i];
+            break;
+        }
+    }
+    if (user) {
+        user.age = userAge;
+        user.name = userName;
+        data = JSON.stringify(users);
+        fs.writeFileSync('data.json', data);
+        res.send(user);
+    } else {
+        res.status(404).send(user);
+    }
 });
 
 app.delete("/api/users/:id", (req, res) => {      
