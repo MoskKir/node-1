201916511858a3ada22e032d8d2b0c9b354f8af3
Kirill Moskalevich <mosk9364@gmail.com>
2020-03-20 15:24:56 +0300

    feat: reduced number of calls to the file system

--- node2/src/services/user-service.js
+++ node2/src/services/user-service.js
@@ -1,71 +1,62 @@
 const fs = require('fs')
 
+let data = fs.readFileSync('../data.json', 'utf8')
 
-const get = async function(){
-    const content = fs.readFileSync('../data.json', 'utf8')
-    const data = JSON.parse(content)
-    return data
+const get = async function() {
+    const userList = JSON.parse(data)
+    return userList
 }
 const add = function (req) {
-    const userName = req.body.name;
-    const userAge = req.body.age;
-    const user = {name: userName, age: userAge};
-     
-    let data = fs.readFileSync('../data.json', 'utf8')
-    let users = JSON.parse(data);
-     
-    // находим максимальный id
+    const userName = req.body.name
+    const userAge = req.body.age
+    const user = {name: userName, age: userAge}
+    
+    let users = JSON.parse(data)
+    
     let id = Math.max.apply(Math, users.map( val => val.id ))
-    // увеличиваем его на единицу
-    user.id = id + 1;
-    // добавляем пользователя в массив
-    users.push(user);
-    data = JSON.stringify(users);
-    // перезаписываем файл с новыми данными
-    fs.writeFileSync('../data.json', data);
+    user.id = id + 1
+
+    users.push(user)
+    data = JSON.stringify(users)
+    fs.writeFileSync('../data.json', data)
     return user
 }
-const update = function(req){
-    const userId = req.body.id;
-    const userName = req.body.name;
-    const userAge = req.body.age;
-     
-    let data = fs.readFileSync('../data.json', 'utf8')
-    const users = JSON.parse(data);
-    let user;
+const update = function(req) {
+    const userId = req.body.id
+    const userName = req.body.name
+    const userAge = req.body.age
+    
+    const users = JSON.parse(data)
+    let user
     for (let i = 0; i < users.length; i++) {
         if (users[i].id === +userId) {
-            user = users[i];
-            break;
+            user = users[i]
+            break
         }
     }
     if (user) {
-        user.age = userAge;
-        user.name = userName;
-        data = JSON.stringify(users);
-        fs.writeFileSync('../data.json', data);
-        res.send(user);
+        user.age = userAge
+        user.name = userName
+        data = JSON.stringify(users)
+        fs.writeFileSync('../data.json', data)
+        return user
     }
 }
-const del = function(req){
+const del = function(req) {
     const id = req.params.id
-    let data = fs.readFileSync('../data.json', 'utf8')
     let users = JSON.parse(data)
-    let index = -1;
-    // находим индекс пользователя в массиве
+    let index = -1
     for (let i = 0; i < users.length; i++) {        
         if (users[i].id === +id) {
-            index = i;            
-            break;
+            index = i            
+            break
         }
     }
     if (index > -1) {
-        // удаляем пользователя из массива по индексу
-        user = users.splice(index, 1)[0];
-        data = JSON.stringify(users);
-        fs.writeFileSync('../data.json', data);
-        // отправляем удаленного пользователя
-        res.send(user);
+        user = users.splice(index, 1)[0]
+        data = JSON.stringify(users)
+        fs.writeFileSync('../data.json', data)
+        return user
     }
 }
 
